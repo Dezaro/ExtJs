83e2b91185dae78bc4bf5e19dd7e5941dd51a67d
evropat <evropat@192.168.2.64>
2016-04-27 14:32:06 +0300

    sdf

--- app/controller/ItemController.js
+++ app/controller/ItemController.js
@@ -33,23 +33,23 @@ Ext.define('Application.controller.ItemController', {
       }
     });
   },
+  tabs: null,
   onItemSelect: function(selModel, selection) {
     // Executed only when selection is a leaf
     //(selection.data.leaf) ? Ext.get('iframe-12').dom.src = selection.raw.description : null;
     this.tabs = Ext.getCmp('tabs12');
     var active = false;
     if(selection.data.leaf) {
-      for(var i = 0; i <  this.tabs.items.length; i++) {
+      for(var i = 0; i < this.tabs.items.length; ++i) {
         if(selection.data.item_id === this.tabs.items.items[i].id) {
-           this.tabs.setActiveTab(i);
-           active = true;
+          this.tabs.setActiveTab(i);
+          active = true;
         }
       }
-      if(active === false){
-           this.tabs.add({id: selection.data.item_id, title: selection.data.name, closable: true, html: selection.data.description});
-           this.tabs.setActiveTab((this.tabs.items.length) - 1);
-         //  console.log(this.tabs.items);
-      }   
+      if(!active) {
+        this.tabs.add({id: selection.data.item_id, title: selection.data.name, closable: true, html: selection.data.description});
+        this.tabs.setActiveTab((this.tabs.items.length) - 1);
+      }
     }
   }
 });
