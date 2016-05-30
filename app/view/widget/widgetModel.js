83c6fe70f9e366f200e087bd826513778c9c70f3
Evropat <Evropat@EvroIT1.evropat.com>
2016-05-30 12:48:08 +0300

    fix widget grid

--- app/view/widget/widgetModel.js
+++ app/view/widget/widgetModel.js
@@ -1,16 +1,16 @@
 Ext.define('Application.view.widget.widgetModel', {
-    extend: 'Application.model.Base',
-    fields: [
-       'name',
-       'progress',
-       'sequence1',
-       'sequence2',
-       'sequence3',
-       'sequence4',
-       'sequence5',
-       'sequence6',
-       'sequence7'
-    ]
+  extend: 'Application.model.Base',
+  fields: [
+    {name: 'name'},
+    {name: 'progress', type: 'float'},
+    'sequence1',
+    'sequence2',
+    'sequence3',
+    'sequence4',
+    'sequence5',
+    'sequence6',
+    'sequence7'
+  ]
 });
 
 
