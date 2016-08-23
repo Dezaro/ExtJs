ac11633cc10b498d868db34943fad847181ed4b8
Evropat <Evropat@EvroIT1.evropat.com>
2016-08-23 16:03:33 +0300

    fix message box in checkTree

--- app/view/checkTree/checkTreeController.js
+++ app/view/checkTree/checkTreeController.js
@@ -3,24 +3,32 @@
 Ext.define('Application.view.checkTree.checkTreeController', {
   extend: 'Ext.app.ViewController',
   alias: 'controller.checkTreeController',
-  onBeforeCheckChange: function(record, checkedState, e){
-    if(record.get('text') === 'Take a nap' && !checkedState){
+  onBeforeCheckChange: function(record, checkedState, e) {
+    if(record.get('text') === 'Take a nap' && !checkedState) {
       Ext.toast('No rest for the wicked!', null, 't');
       return false;
     }
   },
-  onCheckedNodesClick: function(){
+  onCheckedNodesClick: function() {
     var records = this.getView().getChecked(),
             names = [];
-    
-    Ext.Array.each(records, function(rec){
+
+    Ext.Array.each(records, function(rec) {
       names.push(rec.get('text'));
     });
-    
-    Ext.MessageBox.show({
+
+//    Ext.MessageBox.show({
+//      title: 'Selected Nodes',
+//      msg: names.join('<br />'),
+//      icon: Ext.MessageBox.INFO
+//    });
+
+    var selectedNodes = Ext.create('Application.view.custom.customMsgBox', {
+      modal: false,
       title: 'Selected Nodes',
       msg: names.join('<br />'),
       icon: Ext.MessageBox.INFO
     });
+    selectedNodes.show();
   }
 });
